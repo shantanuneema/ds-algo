7b730b8223c9db7f918c7ab1e7ad7228fd77ccc0
Shantanu <shantanuneema@gmail.com>
2021-06-11 22:45:53 -0700

    binary tree validation using inorder Traversal

--- Python/binaryTrees.py
+++ Python/binaryTrees.py
@@ -32,6 +32,40 @@ if __name__ == "__main__":
 In-Order, Pre-Order and Post-Order traversal techniques for Binary Search Trees
 """
 
+"""
+Construction of binary trees in Python
+"""
+
+class TreeNode:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+class BinaryTree:
+
+    def insertLevelOrder(self, arr, root, i, n):
+        if i < n:
+            temp = TreeNode(arr[i]) 
+            root = temp 
+            # insert left child 
+            root.left = self.insertLevelOrder(arr, root.left, 2 * i + 1, n)
+            # insert right child 
+            root.right = self.insertLevelOrder(arr, root.right, 2 * i + 2, n)
+        return root
+
+if __name__ == "__main__":
+    s = BinaryTree()
+    nums = [25,20,30,10,22,28,40,5,15,21,23,27,29,35,50]
+    arr = [1,2,3,4,5]
+    tree_node = s.insertLevelOrder(nums, None, 0, len(nums))
+    new_node = s.insertLevelOrder(arr, None, 0, len(arr))
+    
+"""
+In-Order, Pre-Order and Post-Order traversal techniques for Binary Search Trees
+"""
+
 class Solution:
     
     def __init__(self):
@@ -57,38 +91,38 @@ class Solution:
         
     def inhelper_asc(self, root):
         if root:
-            self.inhelper_asc(self.inhelper_asc(root.left))
+            self.inhelper_asc(root.left)
             self.result.append(root.value)
-            self.inhelper_asc(self.inhelper_asc(root.right))
+            self.inhelper_asc(root.right)
     
     def inhelper_desc(self, root):
         if root:
-            self.inhelper_desc(self.inhelper_desc(root.right))
+            self.inhelper_desc(root.right)
             self.result.append(root.value)
-            self.inhelper_desc(self.inhelper_desc(root.left))
+            self.inhelper_desc(root.left)
             
     def prehelper_asc(self, root):
         if root:
             self.result.append(root.value)
-            self.prehelper_asc(self.prehelper_asc(root.left))
-            self.prehelper_asc(self.prehelper_asc(root.right))
+            self.prehelper_asc(root.left)
+            self.prehelper_asc(root.right)
     
     def prehelper_desc(self, root):
         if root:
             self.result.append(root.value)
-            self.prehelper_desc(self.prehelper_desc(root.right))
-            self.prehelper_desc(self.prehelper_desc(root.left))
+            self.prehelper_desc(root.right)
+            self.prehelper_desc(root.left)
             
     def posthelper_asc(self, root):
         if root:
-            self.posthelper_asc(self.posthelper_asc(root.left))
-            self.posthelper_asc(self.posthelper_asc(root.right))
+            self.posthelper_asc(root.left)
+            self.posthelper_asc(root.right)
             self.result.append(root.value)
     
     def posthelper_desc(self, root):
         if root:
-            self.posthelper_desc(self.posthelper_desc(root.right))
-            self.posthelper_desc(self.posthelper_desc(root.left))
+            self.posthelper_desc(root.right)
+            self.posthelper_desc(root.left)
             self.result.append(root.value)
 
 print(Solution().in_order(tree_node))
@@ -97,3 +131,51 @@ print(Solution().pre_order(tree_node))
 print(Solution().pre_order(tree_node, ascending = False))
 print(Solution().post_order(tree_node))
 print(Solution().post_order(tree_node, ascending = False))
+
+"""
+Validate a Binary-Tree
+"""
+
+# Recursive Solution:
+class Solution:
+    
+    def isValidBST(self, root: TreeNode) -> bool:
+        if root == None: return True
+        self.result = []
+        self.inOrder(root)
+        if len(self.result) > 1:
+            for i in range(1, len(self.result)):
+                if self.result[i - 1] >= self.result[i]:
+                    return False
+        return True
+    
+    def inOrder(self, root):
+        if root:
+            self.inOrder(root.left)
+            self.result.append(root.value)
+            self.inOrder(root.right)
+            
+if __name__ == "__main__":
+    print(Solution().isValidBST(tree_node))
+    print(Solution().isValidBST(new_node))
+    
+# Iterative Solution    
+class Solution:
+    def isValidBST(self, root):
+        stack = []; prev = TreeNode(-float('inf'))
+        while root != None or len(stack) != 0:
+            while root != None:
+                stack.append(root)
+                root = root.left
+            # print([r.value for r in stack])
+            root = stack.pop()
+            if prev != None and root != None:
+                if prev.value >= root.value:
+                    return False
+            prev = root
+            root = root.right
+        return True
+    
+if __name__ == "__main__":
+    print(Solution().isValidBST(tree_node))
+    print(Solution().isValidBST(new_node))
